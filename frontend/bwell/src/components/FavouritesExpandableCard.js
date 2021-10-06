7e7795345cdc55dcd7235e19d4bee54fc463b647
Łukasz Rydzewski <luk.rydzewski@gmail.com>
2021-10-06 20:03:57 +0200

    Working on fav page

--- frontend/bwell/src/components/FavouritesExpandableCard.js
+++ frontend/bwell/src/components/FavouritesExpandableCard.js
@@ -8,13 +8,14 @@ import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { colors, viewportSize } from '../utilities/utilities';
+import { colors, endpoints, viewportSize } from '../utilities/utilities';
 import PropTypes from "prop-types";
 import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import AssignmentReturnedIcon from '@material-ui/icons/AssignmentReturned';
 import FavouritesListElement from './FavouritesListElement';
-
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -81,36 +82,86 @@ const FavouritesExpandableCard = ((props) => {
         setExpanded(!expanded);
     };
 
+    const [ideas, setIdeas] = useState(null);
+
+    let APIurl;
+
+    switch (props.type) {
+        case 'fitWell':
+            APIurl = endpoints.APIfitWell;
+            break;
+        case 'eatWell':
+            APIurl = endpoints.APIeatWell;
+            break;
+        case 'restWell':
+            APIurl = endpoints.APIrestWell;
+            break;
+        case 'thinkWell':
+            APIurl = endpoints.APIthinkWell;
+            break;
+        default:
+            APIurl = '';
+    }
+
+    useEffect(() => {
+
+        const fetchIdeas = async () => {
+            console.log(APIurl);
+            const response = await fetch(endpoints.APIhost + APIurl)
+            const data = await response.json()
+
+            return data
+        }
+
+        const getIdeas = async () => {
+            const ideasFromServer = await fetchIdeas()
+            setIdeas(ideasFromServer)
+        }
+        getIdeas()
+
+        console.log(ideas);
+
+    },[APIurl, ideas]);
+
+    //given I have ids [1,2,3] for favourites in props.favourites. need to retrieve each from API and show title and URL to details
+    // ?id=1
+
     return (
-
-        <Card className={classes.root} >
-            <CardContent>
-                <CardIcon className={classes.cardIcon} spacing={2} />
-                <Typography variant="h2" className={classes.header} component="h1">
-                    {props.title}
-                </Typography>
-                <IconButton
-                    className={clsx(classes.expand, {
-                        [classes.expandOpen]: expanded,
-                    })}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                >
-                    <ExpandMoreIcon />
-                </IconButton>
-            </CardContent>
-
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
+        ideas && ideas.map((ideaId) => {
+            return (
+            <Card className={classes.root} >
                 <CardContent>
-                    <FavouritesListElement />
+                    <CardIcon className={classes.cardIcon} spacing={2} />
+                    <Typography variant="h2" className={classes.header} component="h1">
+                        {props.title}
+                    </Typography>
+                    <IconButton
+                        className={clsx(classes.expand, {
+                            [classes.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </IconButton>
                 </CardContent>
-                <CardContent>
-                    <Button component={Link} to={props.linkTo} variant="outlined" endIcon={<AssignmentReturnedIcon />} className={classes.checkButton} text="check">Remove</Button>
-                    <Button component={Link} to={props.linkTo} variant="outlined" endIcon={<AssignmentReturnedIcon />} className={classes.checkButton} text="check">Back</Button>
-                </CardContent>
-            </Collapse>
-        </Card >
+
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <FavouritesListElement />
+                    </CardContent>
+                    <CardContent>
+                        <Button component={Link} to={props.linkTo} variant="outlined" endIcon={<AssignmentReturnedIcon />} className={classes.checkButton} text="check">Remove</Button>
+                        <Button component={Link} to={props.linkTo} variant="outlined" endIcon={<AssignmentReturnedIcon />} className={classes.checkButton} text="check">Back</Button>
+                    </CardContent>
+                </Collapse>
+            </Card >
+            )
+        }
+
+        )
+
     );
 });
 
--- frontend/bwell/src/components/FavouritesPage.js
+++ frontend/bwell/src/components/FavouritesPage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Grid, makeStyles } from '@material-ui/core'
 import FavouritesExpandableCard from './FavouritesExpandableCard';
 import WeekendOutlinedIcon from '@material-ui/icons/WeekendOutlined';
+import { useEffect, useState } from 'react';
+import { endpoints } from '../utilities/utilities';
 
 const useStyles = makeStyles((theme) => ({
     categoriesBar: {
@@ -19,23 +21,45 @@ const useStyles = makeStyles((theme) => ({
 
 const FavouritesPage = (props) => {
     const classes = useStyles();
+    const loggedUser = 1;
+
+    const APIurl = endpoints.APIhost + endpoints.APIusers;
+
+    const [favourites, setFavourites] = useState(null);
+
+    useEffect(() => {
+
+        const fetchFavourites = async () => {
+            const response = await fetch(`${APIurl}${loggedUser}/${endpoints.APIuserFavourites}`)
+            const data = await response.json()
+    
+            return data 
+        }
+
+        const getIdeas = async () => {
+            const ideasFromServer = await fetchFavourites()
+            setFavourites(ideasFromServer)
+        }
+        getIdeas()
+    },[APIurl, loggedUser]);
 
     return (
+        favourites &&
         <>
             <Grid container spacing={2} xs={12} className={classes.categoriesBar}>
                 <Grid item className={classes.cards} xs={12} md={8}>
                     <Grid container xs={12} spacing={2} className={classes.cards}>
                         <Grid item xs={12} md={6}>
-                            <FavouritesExpandableCard cardIcon={WeekendOutlinedIcon} title="eatWell favourites" />
+                            <FavouritesExpandableCard cardIcon={WeekendOutlinedIcon} title="eatWell favourites" type="eatWell" favourites={favourites.recipes} />
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FavouritesExpandableCard cardIcon={WeekendOutlinedIcon} title="eatWell favourites" />
+                            <FavouritesExpandableCard cardIcon={WeekendOutlinedIcon} title="restWell favourites" type="restWell" favourites={favourites.activities} />
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FavouritesExpandableCard cardIcon={WeekendOutlinedIcon} title="eatWell favourites" />
+                            <FavouritesExpandableCard cardIcon={WeekendOutlinedIcon} title="thinkWell favourites" type="thinkWell" favourites={favourites.ideas} />
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <FavouritesExpandableCard cardIcon={WeekendOutlinedIcon} title="eatWell favourites" />
+                            <FavouritesExpandableCard cardIcon={WeekendOutlinedIcon} title="fitWell favourites" type="fitWell" favourites={favourites.excercises} />
                         </Grid>
                     </Grid>
                 </Grid>
