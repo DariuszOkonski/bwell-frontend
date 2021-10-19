5ea665c457f5b0f3d173506dbac7c285f690f409
Dariusz Okonski <darek200180@gmail.com>
2021-10-19 17:56:20 +0100

    add new endpoint eatwell_ingredient_query

--- frontend/bwell/src/components/eatwell/apiSpoonacular/IngredientHintsFetcher.js
+++ frontend/bwell/src/components/eatwell/apiSpoonacular/IngredientHintsFetcher.js
@@ -1,3 +1,5 @@
+import { endpoints } from "../../../utilities/utilities";
+
 // TODO move API KEYS to backend and provide only for logged users 
 const API_KEY = '1eac70c6679646c495c10b490246131b';
 // const API_KEY2 = '11e7a27c2c6c4d3abb6617b3c7521887';
@@ -10,7 +12,7 @@ export class IngredientsHintsFetcher {
     }
     getURL(query) { 
         // return `http://localhost:8080/api/query?query=${query}`;
-        return `http://localhost/api/eatwell/ingredient/q?query=${query}`;
+        return `${endpoints.eatwell_ingredient_query}${query}`;
         // return `https://api.spoonacular.com/food/ingredients/search?apiKey=${API_KEY}&query=${query}&number=10&metaInformation=true`;
     }
     setPhrase(phrase) {
