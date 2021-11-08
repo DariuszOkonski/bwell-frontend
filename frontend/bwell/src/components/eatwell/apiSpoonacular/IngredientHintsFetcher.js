b1cc84fcf772017e304f3ade4b323f62c4ee7973
Daniel Wątor [KRK NOV 2020] <dmwator11@gmail.com>
2021-11-08 22:52:11 +0100

    update hints fetcher

--- frontend/bwell/src/components/eatwell/apiSpoonacular/IngredientHintsFetcher.js
+++ frontend/bwell/src/components/eatwell/apiSpoonacular/IngredientHintsFetcher.js
@@ -1,9 +1,5 @@
 import { endpoints } from "../../../utilities/utilities";
 
-// TODO move API KEYS to backend and provide only for logged users 
-const API_KEY = '1eac70c6679646c495c10b490246131b';
-// const API_KEY2 = '11e7a27c2c6c4d3abb6617b3c7521887';
-
 export class IngredientsHintsFetcher {
     
     constructor(phrase=null, hints=null){
@@ -12,9 +8,7 @@ export class IngredientsHintsFetcher {
     }
 
     getURL(query) { 
-        // return `http://localhost:8080/api/query?query=${query}`;
         return `${endpoints.eatwell_ingredient_query}${query}`;
-        // return `https://api.spoonacular.com/food/ingredients/search?apiKey=${API_KEY}&query=${query}&number=10&metaInformation=true`;
     }
     setPhrase(phrase) {
         this.phrase = phrase
